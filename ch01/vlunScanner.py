67b104b343e02dd8f53581636b052d8d03ce01fa
takuzoo <sata3868.k.serpentis@gmail.com>
2018-05-26 10:33:51 +0900

    :+1: update vlunScanner function

--- ch01/vlunScanner.py
+++ ch01/vlunScanner.py
@@ -1,79 +1,76 @@
 #!/usr/bin/env python
-"""
-Usage: python vlunScanner.py <vulnerability_list_filename>
-"""
 import socket
-import sys
-import os
+import optparse
 
 
-def get_banner(ip, portlist):
-    """
-    Scan for and return banners(first 1024 bytes message) from anIP across a port range.
-
-    :return: Dictionary -- {PORT:BANNER}
-    """
-    socket.setdefaulttimeout(2)
-    banner = {}
-    for port in portlist:
-        try:
-            s = socket.socket()
-            print('Grabbing banner from {}:{}'.format(str(ip), port))
-            s.connect((ip, port))
-            banner[port] = s.recv(1024)
-            s.detach()
-        except:
-            continue
-    return banner
+def get_banner(ip, port, timeout):
+    socket.setdefaulttimeout(timeout)
+    s = socket.socket()
+    try:
+        print('[+] Grabbing banner from {}:{}'.format(str(ip), port))
+        s.connect((ip, port))
+        ans = s.recv(1024)
+        return ans
+    except Exception as e:
+        print("[-] Error {}:{} = {}".format(ip, port, e))
+        return None
 
 
-def check_vulns(ip, port, filename, banner):
+def check_vulnerabilities(banner, filename):
     """
     Check for known vulnerable services against a pre-defined list of banners.
 
-    :param ip: IP address being scanned; type: str
-    :param port: Port being scanned
-    :param filename: List of vulnerability banners
     :param banner: Banner being checked; type: str
-    :return: 0
+    :param filename: List of vulnerability banners
     """
     with open(filename, 'r') as f:
         for line in f.readlines():
             if line.strip('\n') in banner:
-                print('[+] IP: {} PORT: {}'.format(ip, port))
                 print("[+] Server is vulnerable: {}".format(banner.strip('\n')))
             else:
                 print("[-] Server is not vulnerable")
                 continue
-    return 0
 
 
 def main():
-    if len(sys.argv) == 2:
-        vuln_list = sys.argv[1]
-        if not os.path.isfile(vuln_list):
-            print('Vulnerability list ({}) missing'.format(vuln_list))
-            exit(1)
-        elif not os.access(vuln_list, os.R_OK):
-            print('Access to Vulnerability List File Denied')
-            exit(1)
-        else:
-            print('Scanning IP range 10.10.10.(1 - 255)')
-            port_list = list(range(1, 1001))
-            banner = {}
+    parser = optparse.OptionParser('usage %prog -n <network> -t <type of probe>')
 
-            for i in range(1, 255):
-                ip = '10.10.10.' + str(i)  # Change to appropriate IP range
-                banner[ip] = get_banner(ip, port_list)
+    """
+    options default
+    ---------------
+    network     : 192.168.1.x
+    subnet start: 1
+    subnet end  : 254
+    ports       : telnet, ssh, smtp, http, imap and https
+    vlun list   : banners.txt
+    timeout     : 2   
+    """
+    parser.add_option('-n', dest='network', type='string', default="192.168.1.X", help='specify network to search on')
+    parser.add_option('--start_subnet', dest='start_subnet', type='int', default=1,
+                      help='specify which subnet should the scan start')
+    parser.add_option('--end_subnet', dest='end_subnet', type='int', default=254,
+                      help='specify which subnet should the scan stop')
+    parser.add_option('-p', dest='ports', type='string', default="21, 22, 25, 80, 110, 443",
+                      help='specify list of ports, separed by comma')
+    parser.add_option('--vul_filename', dest='vulnerabilities_filename', type='string', default="banners.txt",
+                      help='default file with list of vulnerabilities to compare')
+    parser.add_option('--socket_timeout', dest='socket_timeout', type='int', default=2,
+                      help='default socket connection timeout')
+    (options, args) = parser.parse_args()
 
-            for ip in list(banner.keys()):
-                for port in banner[ip].keys():
-                    check_vulns(ip, port, vuln_list, str(banner[ip][port]))
-    else:
-        print('Usage: python vlunScanner.py <vulnerability_list_filename>')
-        exit(1)
+    subnet = options.network.lower()
+    subnet_string = subnet.replace("x", "{}")
+    ip_list = map(lambda ip: subnet_string.format(ip), range(options.start_subnet, options.end_subnet))
+    port_list = map(int, filter(None, map(lambda p: p.strip(), options.ports.split(","))))
 
-    return 0
+    print("[*] Testing subnet of {} for {} ports: {}".format(subnet, len(list(port_list)), options.ports))
+    for ip in ip_list:
+        for port in options.ports.split(","):
+            port_int = int(port)
+            banner = get_banner(ip, port_int, timeout=options.socket_timeout)
+            if banner:
+                print("[+] Checking {}:{}".format(ip, port))
+                check_vulnerabilities(banner, filename=options.vul_filename)
 
 
 if __name__ == '__main__':
